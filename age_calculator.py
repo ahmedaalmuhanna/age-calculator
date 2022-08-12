dce5a3f669b223a6709841cd6740e937ed2705ff
Ahmed <ahmed.a.almuhanna@hotmail.com>
2022-08-12 16:52:52 +0300

    age calculating task

--- age_calculator.py
+++ age_calculator.py
@@ -1,19 +1,32 @@
-from datetime import date
+from datetime import date, timedelta
 
-
+import datetime
 def get_dob():
+    
     # write code here
-	...
+	bdy = input("Enter year of birth: ")
+	bdm = input("Enter month of birth: ")
+	bdd = input("Enter day of birth: ")
+	print(type(int(bdy)))
+	if bdy.isdigit() and bdm.isdigit() and bdd.isdigit() :
+			print(type(date.today()))
+			
+			print(datetime.date(int(bdy),int(bdm),int(bdd)))
+   
+			return datetime.date(int(bdy),int(bdm),int(bdd))
+	else:
+			print("your input is invalid")
+			return 0
+
+def get_age(date):
+	print(date)
 
 
-def get_age(dob):
-    # write code here
-	...
-
+	return date.today()- date
 
 def main():
-	# write code here
-	...
+
+	print(get_age(get_dob()))
 
 
 if __name__ == '__main__':
